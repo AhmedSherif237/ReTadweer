50ba7d414051e5ad750ac315d437082a7587facf
AhmedSherif237 <sh0803970@gmail.com>
2024-09-18 07:20:23 -0700

    Update app.js

--- login/app.js
+++ login/app.js
@@ -9,3 +9,29 @@ sign_up_btn.addEventListener('click', () =>{
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const signInForm = document.querySelector('.sign-in-form');
+    const signUpForm = document.querySelector('.sign-up-form');
+  
+    if (signInForm) {
+      signInForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // منع النموذج من الإرسال الافتراضي
+        // تنفيذ عملية تسجيل الدخول هنا
+  
+        // إعادة توجيه المستخدم إلى index.html بعد تسجيل الدخول
+        window.location.href = '../index.html';
+      });
+    }
+  
+    if (signUpForm) {
+      signUpForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // منع النموذج من الإرسال الافتراضي
+        // تنفيذ عملية التسجيل هنا
+  
+        // إعادة توجيه المستخدم إلى index.html بعد التسجيل
+        window.location.href = '../index.html';
+      });
+    }
+  });
+  
